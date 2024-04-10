b02bb8ded0ae584f874a97b49b979736b468d76a
Arnab-NCI <x22107321@student.ncirl.ie>
2024-04-10 18:25:21 +0530

    folder structure added

---
+++ templet.py
@@ -0,0 +1,60 @@
+''' Inside the templet we write the project structure, when I will run this file,
+    required files and folders will be created automatically.'''
+
+# Import necessary modules
+import os
+from pathlib import Path
+import logging
+
+# Configure logging with INFO level and a specific format
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+# Define the project name
+project_name = "textSummarizer"
+
+# Define a list of files to be created
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+
+]
+
+
+# Iterate through each file path in the list
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # Check if the directory is not empty
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    # Check if the file doesn't exist or is empty
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
